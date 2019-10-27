7d394dd21537eec560607d7e40f7c76a5983e8a8
Diego Avila <diegob.avila@hotmail.com>
2019-10-27 17:17:53 -0300

    Criado as estruturas de page, corrigidos problemas de versão do react-navigation, react-navigation-stack e react-native-gesture-handler. Agora é só seguir o desenvolvimento.

--- App.js
+++ App.js
@@ -1,61 +1,16 @@
-import React, {Component} from 'react';
-import { View, Text } from 'react-native';
-import axios from 'axios';
+import React from 'react'
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-import Header from './src/components/Header';
-import PeopleList from './src/components/PeopleList'
+import PeoplePage from './src/pages/PeoplePage';
 
-/*
-  Classe principal da aplicação
-*/
-
-export default class App extends Component {
-  /*
-    Definimos que ao executar o app criamos um state com array de peoples vazio
-  */
-  constructor(props){
-    super(props);
-    this.state = {
-      peoples: [
-      ]
-    }
+const AppNavigator = createStackNavigator({
+  'Main': {
+    screen: PeoplePage
   }
-  /*
-    Função nativa do React que se executa ao iniciar a classe e após o render()
-    Aqui ele traz os dados das pessoas da api https://randomuser.me com o Axios
-    e seta no state atravéz do setState()
-  */
-  componentDidMount(){
-    axios.get('https://randomuser.me/api/?nat=br&results=5')
-    .then(response => {
-      const {results} =  response.data;
-      this.setState({
-        peoples: results
-      })
-    });
-  }
+});
 
+const AppContainer = createAppContainer(AppNavigator);
 
-  /*
-    Função com a responsabilidade de transformar as as informações do state em lista de pessoas
-    retornando uma lista pronta
-  */
-  renderList(){
-   
-   return textElements;
-  }
-
-  /*
-    Função responsavel por exibir na tela as informações
-  */
-  render(){
-    return (
-      <View>
-        <Header title="Header Pessoa" />
-        <PeopleList peoples={this.state.peoples}  />
-      </View>
-    );
-  }
-}
-
+export default AppContainer;
 
---
+++ src/pages/PeoplePage.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+import Header from '../components/Header';
+import PeopleList from '../components/PeopleList'
+
+/*
+  Classe principal da aplicação
+*/
+export default class PeoplePage extends React.Component {
+  /*
+    Definimos que ao executar o app criamos um state com array de peoples vazio
+  */
+  constructor(props){
+    super(props);
+    this.state = {
+      peoples: [
+
+      ]
+    }
+  }
+  /*
+    Função nativa do React que se executa ao iniciar a classe e após o render()
+    Aqui ele traz os dados das pessoas da api https://randomuser.me com o Axios
+    e seta no state atravéz do setState()
+  */
+  componentDidMount(){
+    axios.get('https://randomuser.me/api/?nat=br&results=5')
+    .then(response => {
+      const {results} =  response.data;
+      this.setState({
+        peoples: results
+      })
+    });
+  }
+  /*
+    Função com a responsabilidade de transformar as as informações do state em lista de pessoas
+    retornando uma lista pronta
+  */
+  renderList(){ 
+   return textElements;
+  }
+
+  /*
+    Função responsavel por exibir na tela as informações
+  */
+  render(){
+    return (
+      <View>
+        <Header title="Header Pessoa" />
+        <PeopleList peoples={this.state.peoples}  />
+      </View>
+    );
+  }
+}
+
+
