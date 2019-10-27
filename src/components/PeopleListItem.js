dac027b2f3e562daf24158bd34750fef5910e2f3
Diego Avila <diegob.avila@hotmail.com>
2019-10-27 15:17:25 -0300

    Deixei a lista clicavel com o component do React TouchableOpacity para que possa clicar na pessoa e direcionar para outra pagina no futuro

--- src/components/PeopleListItem.js
+++ src/components/PeopleListItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {Text, View, StyleSheet, Image} from 'react-native';
+import {Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import { capitalizeFirstLetter } from '../util'
+
+
 
 /*
     Componente responsavel por receber da PeopleList uma pessoa, 
@@ -10,12 +12,14 @@ const PeopleListItem = props => {
     const { people } = props;
     const { title, first, last } = people.name;
     return (
+        <TouchableOpacity onPress={() => console.log("Clicou")} >
             <View style={styles.line}>
                 <Image style={styles.avatar} source={{uri: people.picture.thumbnail}} />
                 <Text style={styles.lineText}>
                     {`${capitalizeFirstLetter(title)} ${capitalizeFirstLetter(first)} ${capitalizeFirstLetter(last)}`}
                 </Text>
             </View>
+        </TouchableOpacity>
     );
 }
 
