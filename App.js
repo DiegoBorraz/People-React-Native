ddd414855dc0b4ff01f26d1afcd87ba646f71603
Diego Avila <diegob.avila@hotmail.com>
2019-10-28 21:03:38 -0300

    Header criado anteriormente foi retirado e agora estou usando o Header nativo do StackNavigator

--- App.js
+++ App.js
@@ -7,6 +7,21 @@ import PeoplePage from './src/pages/PeoplePage';
 const AppNavigator = createStackNavigator({
   'Main': {
     screen: PeoplePage
+  },
+}, {
+  defaultNavigationOptions: {
+    title: 'Pessoas!',
+    headerStyle: {
+      backgroundColor: '#6ca2f7',
+      borderBottomWidth: 1,
+      borderBottomColor: '#C5C5C5'
+    },
+    headerTitleStyle: {
+      color: 'white',
+      fontSize: 30,
+      flexGrow:1,
+      textAlign: 'center',
+    }
   }
 });
 
--- src/pages/PeoplePage.js
+++ src/pages/PeoplePage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
 
-import Header from '../components/Header';
 import PeopleList from '../components/PeopleList'
 
 /*
@@ -48,7 +47,6 @@ export default class PeoplePage extends React.Component {
   render(){
     return (
       <View>
-        <Header title="Header Pessoa" />
         <PeopleList peoples={this.state.peoples}  />
       </View>
     );
