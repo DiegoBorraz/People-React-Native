f6dc65e91a35519a21982163be987651a57f3df3
Diego Avila <diegob.avila@hotmail.com>
2019-10-29 15:28:25 -0300

    Foi criado o component Line para cada linha das informações da Pessoa exibida

---
+++ src/components/Line.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+/*
+    Componente responsavel pelas linhas da PeopleDetailPage
+*/
+const Line = ({ label = "" , content = ""}) => { // default = "" previne erro de campo null
+    return (
+        <View style={ styles.line }>
+            <Text style={ [
+                styles.cell,
+                styles.label,
+                label.length > 8 ? styles.longLabel : null // Verifica a quantidade de caracteres, se for maior de 8, adiciona o style do longLabel
+            ]}>{ label }</Text>
+            <Text style={ [styles.cell, styles.content]}>{ content }</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    line: {
+        flexDirection: 'row',
+        paddingTop: 3,
+        paddingBottom: 3,
+        borderWidth:1,
+        borderColor: '#C5C5C5'
+    },
+    cell: {
+        fontSize: 18,
+        paddingLeft: 5,
+        //borderWidth: 1
+    },
+    label: {
+        fontWeight: 'bold',
+        flex: 1
+    },
+    content: {
+        flex: 3
+    },
+    longLabel: {
+        fontSize: 12
+    }
+});
+
+export default Line;
